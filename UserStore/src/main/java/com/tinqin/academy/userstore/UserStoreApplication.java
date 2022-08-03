68a7ce95b27cca468d777824aafae8a861833065
Nikola Slavchev <nslavchev48@gmail.com>
2022-08-03 12:52:56 +0300

    Init userStore project

---
+++ UserStore/src/main/java/com/tinqin/academy/userstore/UserStoreApplication.java
@@ -0,0 +1,13 @@
+package com.tinqin.academy.userstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserStoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UserStoreApplication.class, args);
+    }
+
+}
---
+++ UserStore/src/test/java/com/tinqin/academy/userstore/UserStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tinqin.academy.userstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserStoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
