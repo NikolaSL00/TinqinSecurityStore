24d7cedfd0061bc68194c700528801208c4a5382
Nikola Slavchev <nslavchev48@gmail.com>
2022-08-23 11:50:16 +0300

    updated test

--- UserStore/src/test/java/com/tinqin/academy/userstore/domain/service/UserServiceImplTest.java
+++ UserStore/src/test/java/com/tinqin/academy/userstore/domain/service/UserServiceImplTest.java
@@ -14,9 +14,6 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.annotation.Import;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 import java.util.Optional;
